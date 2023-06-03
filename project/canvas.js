ba15e62441aa068376c86253b49cf315b4479d30
Gustav <gustav@kramnas.se>
2023-06-04 00:58:33 +0200

    new branch commit

--- project/canvas.js
+++ project/canvas.js
@@ -1,5 +1,4 @@
-import { Application } from '@splinetool/runtime';
-
+"use strict";
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
 var cH;
@@ -8,20 +7,20 @@ var bgColor = "#FF6138";
 var animations = [];
 var circles = [];
 
-var colorPicker = (function() {
-  var colors = ["#FF6138", "#FFBE53", "#2980B9", "#575587"];
+var colorPicker = (function () {
+  var colors = ["#FF6138", "#FFBE53", "#2980B9", "#282741"];
   var index = 0;
   function next() {
-    index = index++ < colors.length-1 ? index : 0;
+    index = index++ < colors.length - 1 ? index : 0;
     return colors[index];
   }
   function current() {
-    return colors[index]
+    return colors[index];
   }
   return {
     next: next,
-    current: current
-  }
+    current: current,
+  };
 })();
 
 function removeAnimation(animation) {
@@ -38,96 +37,96 @@ function calcPageFillRadius(x, y) {
 function addClickListeners() {
   document.addEventListener("onClick", handleEvent);
   document.addEventListener("mousedown", handleEvent);
-};
-
-function handleEvent(e) {
-    if (e.touches) { 
-      e.preventDefault();
-      e = e.touches[0];
-    }
-    var currentColor = colorPicker.current();
-    var nextColor = colorPicker.next();
-    var targetR = calcPageFillRadius(e.pageX, e.pageY);
-    var rippleSize = Math.min(200, (cW * .4));
-    var minCoverDuration = 750;
-    
-    var pageFill = new Circle({
-      x: e.pageX,
-      y: e.pageY,
-      r: 0,
-      fill: nextColor
-    });
-    var fillAnimation = anime({
-      targets: pageFill,
-      r: targetR,
-      duration:  Math.max(targetR / 2 , minCoverDuration ),
-      easing: "easeOutQuart",
-      complete: function(){
-        bgColor = pageFill.fill;
-        removeAnimation(fillAnimation);
-      }
-    });
-    
-    var ripple = new Circle({
-      x: e.pageX,
-      y: e.pageY,
-      r: 0,
-      fill: currentColor,
-      stroke: {
-        width: 3,
-        color: currentColor
-      },
-      opacity: 1
-    });
-    var rippleAnimation = anime({
-      targets: ripple,
-      r: rippleSize,
-      opacity: 0,
-      easing: "easeOutExpo",
-      duration: 900,
-      complete: removeAnimation
-    });
-    
-    var particles = [];
-    for (var i=0; i<32; i++) {
-      var particle = new Circle({
-        x: e.pageX,
-        y: e.pageY,
-        fill: currentColor,
-        r: anime.random(24, 48)
-      })
-      particles.push(particle);
-    }
-    var particlesAnimation = anime({
-      targets: particles,
-      x: function(particle){
-        return particle.x + anime.random(rippleSize, -rippleSize);
-      },
-      y: function(particle){
-        return particle.y + anime.random(rippleSize * 1.15, -rippleSize * 1.15);
-      },
-      r: 0,
-      easing: "easeOutExpo",
-      duration: anime.random(1000,1300),
-      complete: removeAnimation
-    });
-    animations.push(fillAnimation, rippleAnimation, particlesAnimation);
 }
 
-function extend(a, b){
-  for(var key in b) {
-    if(b.hasOwnProperty(key)) {
+function handleEvent(e) {
+  if (e.touches) {
+    e.preventDefault();
+    e = e.touches[0];
+  }
+  var currentColor = colorPicker.current();
+  var nextColor = colorPicker.next();
+  var targetR = calcPageFillRadius(e.pageX, e.pageY);
+  var rippleSize = Math.min(200, cW * 0.4);
+  var minCoverDuration = 750;
+
+  var pageFill = new Circle({
+    x: e.pageX,
+    y: e.pageY,
+    r: 0,
+    fill: nextColor,
+  });
+  var fillAnimation = anime({
+    targets: pageFill,
+    r: targetR,
+    duration: Math.max(targetR / 2, minCoverDuration),
+    easing: "easeOutQuart",
+    complete: function () {
+      bgColor = pageFill.fill;
+      removeAnimation(fillAnimation);
+    },
+  });
+
+  var ripple = new Circle({
+    x: e.pageX,
+    y: e.pageY,
+    r: 0,
+    fill: currentColor,
+    stroke: {
+      width: 3,
+      color: currentColor,
+    },
+    opacity: 1,
+  });
+  var rippleAnimation = anime({
+    targets: ripple,
+    r: rippleSize,
+    opacity: 0,
+    easing: "easeOutExpo",
+    duration: 900,
+    complete: removeAnimation,
+  });
+
+  var particles = [];
+  for (var i = 0; i < 32; i++) {
+    var particle = new Circle({
+      x: e.pageX,
+      y: e.pageY,
+      fill: currentColor,
+      r: anime.random(24, 48),
+    });
+    particles.push(particle);
+  }
+  var particlesAnimation = anime({
+    targets: particles,
+    x: function (particle) {
+      return particle.x + anime.random(rippleSize, -rippleSize);
+    },
+    y: function (particle) {
+      return particle.y + anime.random(rippleSize * 1.15, -rippleSize * 1.15);
+    },
+    r: 0,
+    easing: "easeOutExpo",
+    duration: anime.random(1000, 1300),
+    complete: removeAnimation,
+  });
+  animations.push(fillAnimation, rippleAnimation, particlesAnimation);
+}
+
+function extend(a, b) {
+  for (var key in b) {
+    if (b.hasOwnProperty(key)) {
       a[key] = b[key];
     }
   }
   return a;
 }
 
-var Circle = function(opts) {
+var Circle = function (opts) {
   extend(this, opts);
-}
+};
 
-Circle.prototype.draw = function() {
+Circle.prototype.draw = function () {
   ctx.globalAlpha = this.opacity || 1;
   ctx.beginPath();
   ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
@@ -142,22 +141,22 @@ Circle.prototype.draw = function() {
   }
   ctx.closePath();
   ctx.globalAlpha = 1;
-}
+};
 
 var animate = anime({
   duration: Infinity,
-  update: function() {
+  update: function () {
     ctx.fillStyle = bgColor;
     ctx.fillRect(0, 0, cW, cH);
-    animations.forEach(function(anim) {
-      anim.animatables.forEach(function(animatable) {
+    animations.forEach(function (anim) {
+      anim.animatables.forEach(function (animatable) {
         animatable.target.draw();
       });
     });
-  }
+  },
 });
 
-var resizeCanvas = function() {
+var resizeCanvas = function () {
   cW = window.innerWidth;
   cH = window.innerHeight;
   c.width = cW * devicePixelRatio;
@@ -170,7 +169,7 @@ var resizeCanvas = function() {
   if (window.CP) {
     // CodePen's loop detection was causin' problems
     // and I have no idea why, so...
-    window.CP.PenTimer.MAX_TIME_IN_LOOP_WO_EXIT = 6000; 
+    window.CP.PenTimer.MAX_TIME_IN_LOOP_WO_EXIT = 6000;
   }
   window.addEventListener("resize", resizeCanvas);
   addClickListeners();
@@ -181,23 +180,26 @@ var resizeCanvas = function() {
 })();
 
 function handleInactiveUser() {
-  var inactive = setTimeout(function(){
-    fauxClick(cW/2, cH/2);
+  var inactive = setTimeout(function () {
+    fauxClick(cW / 2, cH / 2);
   }, 2000);
-  
+
   function clearInactiveTimeout() {
     clearTimeout(inactive);
     document.removeEventListener("mousedown", clearInactiveTimeout);
     document.removeEventListener("touchstart", clearInactiveTimeout);
   }
-  
+
   document.addEventListener("mousedown", clearInactiveTimeout);
   document.addEventListener("touchstart", clearInactiveTimeout);
 }
 
 function startFauxClicking() {
-  setTimeout(function(){
-    fauxClick(anime.random( cW * .2, cW * .8), anime.random(cH * .2, cH * .8));
+  setTimeout(function () {
+    fauxClick(
+      anime.random(cW * 0.2, cW * 0.8),
+      anime.random(cH * 0.2, cH * 0.8)
+    );
     startFauxClicking();
   }, anime.random(200, 900));
 }
@@ -206,21 +208,5 @@ function fauxClick(x, y) {
   var fauxClick = new Event("mousedown");
   fauxClick.pageX = x;
   fauxClick.pageY = y;
-  document.dispatchEvent(fauxClick)
+  document.dispatchEvent(fauxClick);
 }
-const canvas = document.getElementById('d');
-
-const app = new Application(canvas);
-app.load('https://prod.spline.design/K1GfqOtiHZjbqb2r/scene.splinecode');
-
-const audio=new Audio('./public/sound.wav');
-const ball = document.querySelector(".soccer");
-
-ball.addEventListener("click", () => {
-  audio.currentTime = 0;
-
-  for (let i = 0; i < 2; i++) {
-    audio.play();
-  }
-
-});
