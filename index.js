8a15152cb8eb0e76f2f2f1496f26dae7218c970e
Gustav <gustav@kramnas.se>
2023-06-12 16:59:51 +0200

    last edits

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-$(document).ready(function() {
-    // Load the content of each section
-    $('#home').load('home.html');
-    setTimeout(function() { $('#line-ups').load('page.html');},500)
-   
-    $('#info').load('section.html');
-    $('#contact').load('contact.html');
-  });
-  
-  $(document).ready(function() {
-    // Reveal sections on scroll
-    $(window).scroll(function() {
-      var scrollTop = $(window).scrollTop();
-      var windowHeight = $(window).height();
-  
-      // Calculate the threshold for revealing a section
-      var threshold = windowHeight * 0.8;
-  
-      // Check if each section is in the viewport and reveal it
-      $('.content').each(function() {
-        var offsetTop = $(this).offset().top;
-  
-        if (scrollTop + windowHeight > offsetTop + threshold) {
-          $(this).addClass('revealed');
-        }
-      });
+$(document).ready(function () {
+  // Load the content of each section
+  $("#home").load("home.html");
+  setTimeout(function () {
+    // $("#line-ups").load("page.html");
+  }, 500);
+
+  $("#info").load("section.html");
+});
+
+$(document).ready(function () {
+  // Reveal sections on scroll
+  $(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+    var windowHeight = $(window).height();
+
+    // Calculate the threshold for revealing a section
+    var threshold = windowHeight * 0.8;
+
+    // Check if each section is in the viewport and reveal it
+    $(".content").each(function () {
+      var offsetTop = $(this).offset().top;
+
+      if (scrollTop + windowHeight > offsetTop + threshold) {
+        $(this).addClass("revealed");
+      }
     });
   });
-  
+});
+
 //   // Add event listeners
 //   document.addEventListener('scroll', handleScroll);
 //   var navLinks = document.querySelectorAll('nav a');
